9293400f06ba1c8ce3e738fd659c2240a45701f6
LukeidDev <luccasdaniel92@gmail.com>
2023-05-29 14:46:08 -0300

    atualizar e buscar

--- src/main/java/ssp/dev/celularseguro/controller/CelularController.java
+++ src/main/java/ssp/dev/celularseguro/controller/CelularController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +38,34 @@ public class CelularController {
 
         Celular celul = celularRepository.save(celular);
         return new ResponseEntity<Celular>(celul, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "delete")
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam Long idcelul){
+
+        celularRepository.deleteById(idcelul);
+        return new ResponseEntity<String>("Aparelho deletado com sucesso", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "buscarcelulid")
+    @ResponseBody
+    public ResponseEntity<Celular> buscarcelulid(@RequestParam(name = "idcelul") Long idcelul){
+
+        Celular celular = celularRepository.findById(idcelul).get();
+        return new ResponseEntity<Celular>(celular, HttpStatus.OK);
+    }
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Celular celular){
+
+
+        if(celular.getId() == null){
+            return new ResponseEntity<String>("Id não foi informado.", HttpStatus.OK);
+        }
+
+        Celular celul = celularRepository.saveAndFlush(celular);
+        return new ResponseEntity<Celular>(celul, HttpStatus.OK);
     }
 
 }
